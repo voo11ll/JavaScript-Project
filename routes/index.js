e905ea379fd0bd1b4fbb947bc4235d2dacb8cfe0
vrungel <arsenyan.m.i20@gmail.com>
2022-06-26 21:25:42 +0300

    12.3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
-
+var User = require("../models/User").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,5 +19,31 @@ router.get('/', function(req, res, next) {
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
+
+
+
+ /* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+      if(user.checkPassword(password)){
+        req.session.user = user
+        res.redirect('/')
+      } else {
+        res.render('logreg', {title: 'Вход', error: 'Пароль не верный'})
+      }
+    } else {
+      var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user
+                res.redirect('/')
+            })     
+    }
+})
+});
 
 module.exports = router;
